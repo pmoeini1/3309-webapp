735c73a12da5a24c631b284f21763d0f5e803702
Alex Salsali <asalsali@uwo.ca>
2021-12-02 17:42:55 -0500

    query fix

---
+++ src/connection.js
@@ -0,0 +1,36 @@
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Salsa7706',
+    database : 'scouting'
+    });
+
+function connect(){
+
+    var connection=null;
+    try{
+
+    
+     // connect to DB
+     connection = mysql.createConnection({
+     host     : 'localhost',
+     user     : 'root',
+     password : 'Salsa7706',
+     database : 'scouting'
+     });
+    }
+    
+    catch(e){
+        console.log(e);
+    }
+
+    finally {
+        console.log(connection)
+    }
+    return connection;
+}
+
+
+module.exports = connection;
