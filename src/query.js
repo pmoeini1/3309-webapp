b1f56231c9cbd20cb55623aca4e7f6679774ad4c
pmoeini1 <pmoeini@uwo.ca>
2021-12-02 16:34:45 -0500

    more little fix

--- src/query.js
+++ src/query.js
@@ -18,19 +18,19 @@ export default function Query(action, data) {
             "' AND t.tName = a.tName;");
         }
         // find elite players by position query
-        if (action===2) {
+        else if (action===2) {
             result = connection.query("SELECT a.playerNo, a.aName, a.aPosition, t.tName, t.tConference, t.ranking, c.cityID, c.cityName, c.country FROM Athlete a, Team t, City c WHERE t.ranking <= 100 AND  t.cityID = c.cityID AND t.tName = a.tName AND t.tConference = a.tConference AND a.aPosition = '" + data + "';");
         }
         // find big 10 centers by height
-        if (action===3) {
+        else if (action===3) {
             result = connection.query("SELECT * FROM Athlete WHERE tConference='BIG 10' AND height=" + data + ';');
         }
         // find athletes over a specific age
-        if (action===4) {
+        else if (action===4) {
             result = connection.query("SELECT * FROM Athlete a WHERE a.age = "+ data + ";")
         }
         // find athletes with a specific name
-        if (action===5) {
+        else if (action===5) {
             result = connection.query("SELECT * FROM ATHLETE a WHERE a.aName = '" + data + "'");
 
         }
